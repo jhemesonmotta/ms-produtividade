22e06dc8e58ab13dbb9ab8d78978f1cb163da85d
Jhemeson Mota <jhemeson.mota@wipro.com>
2020-12-09 17:35:02 -0300

    repository para alocacao

---
+++ src/main/java/com/jhemeson/msusuarios/repository/AlocacaoRepository.java
@@ -0,0 +1,8 @@
+package com.jhemeson.msusuarios.repository;
+
+import com.jhemeson.msusuarios.entity.Alocacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AlocacaoRepository extends JpaRepository<Alocacao, Long>  {
+
+}
