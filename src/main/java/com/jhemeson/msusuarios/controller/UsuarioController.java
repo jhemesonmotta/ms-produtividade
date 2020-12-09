f3c7510931d722abab706bc4b0723f7549f782a5
Jhemeson Mota <jhemeson.mota@wipro.com>
2020-12-09 12:12:04 -0300

    http services de criar e atualizar usuario

--- src/main/java/com/jhemeson/msusuarios/controller/UsuarioController.java
+++ src/main/java/com/jhemeson/msusuarios/controller/UsuarioController.java
@@ -1,13 +1,11 @@
 package com.jhemeson.msusuarios.controller;
 
+import com.jhemeson.msusuarios.dto.MessageResponseDTO;
 import com.jhemeson.msusuarios.dto.UsuarioDTO;
 import com.jhemeson.msusuarios.service.UsuarioService;
 import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/usuarios")
@@ -24,4 +22,15 @@ public class UsuarioController {
     public UsuarioDTO buscarUsuarioPorId(@PathVariable Long id) throws NotFoundException {
         return usuarioService.findById(id);
     }
+
+    @PostMapping
+    public MessageResponseDTO adicionarUsuario(@RequestBody UsuarioDTO usuarioDTO) {
+        return usuarioService.create(usuarioDTO);
+    }
+
+    @PutMapping
+    public MessageResponseDTO atualizarUsuario(@RequestBody UsuarioDTO usuarioDTO) {
+        return usuarioService.update(usuarioDTO);
+    }
+
 }
