3b414a2a90ab0d845c91bef1ea980e59e4c53b43
Jhemeson Mota <jhemeson.mota@wipro.com>
2020-12-09 17:32:13 -0300

    mapper para alocacao

---
+++ src/main/java/com/jhemeson/msusuarios/mapper/AlocacaoMapper.java
@@ -0,0 +1,13 @@
+package com.jhemeson.msusuarios.mapper;
+
+import com.jhemeson.msusuarios.dto.AlocacaoDTO;
+import com.jhemeson.msusuarios.entity.Alocacao;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface AlocacaoMapper {
+    AlocacaoMapper INSTANCE = Mappers.getMapper(AlocacaoMapper.class);
+    Alocacao toModel(AlocacaoDTO alocacaoDTO);
+    AlocacaoDTO toDTO(Alocacao alocacao);
+}
