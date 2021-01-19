45d2c69a1e3af05b2b0a2630707e41d7bee1cbd1
jhemesonmotta <jhemesonmotta@gmail.com>
2021-01-19 11:24:36 -0300

    add entity fator

---
+++ src/main/java/com/jhemeson/msprodutividade/entity/Fator.java
@@ -0,0 +1,36 @@
+package com.jhemeson.msprodutividade.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Fator {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(unique = true, nullable = false)
+	private String nome;
+
+	@Column(nullable = false)
+	private String descricao;
+
+	@Column
+	private String tipoDeUsuarioPreenchendo;
+
+	@Column
+	private double pesoDefault;
+
+	@Column(nullable = false)
+	private String categoria;
+
+	@Column
+	private String textoDoInputTextual;
+}
