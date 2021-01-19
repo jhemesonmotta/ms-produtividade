19748885995e8653239dca6582497beef5323cd9
jhemesonmotta <jhemesonmotta@gmail.com>
2021-01-19 11:53:35 -0300

    add entity MedicaoPorPessoa

--- src/main/java/com/jhemeson/msprodutividade/entity/MedicaoPorPessoa.java
+++ src/main/java/com/jhemeson/msprodutividade/entity/MedicaoPorPessoa.java
@@ -12,20 +12,17 @@ import javax.persistence.*;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class FatorMedidoPorPessoa {
+public class MedicaoPorPessoa {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@Column(nullable = false, name = "fator_id")
-	private Long fatorId;
-
-	@Column(nullable = false, name = "medicao_pessoa_id")
-	private Long medicaoPorPessoaId;
-
 	@Column(nullable = false)
-	private double nota;
+	private String data;
 
-	@Column(nullable = false)
-	private String comentarios;
+	@Column(nullable = false, name = "medicao_empresa_id")
+	private Long medicaoEmpresaId;
+
+	@Column(nullable = false, name = "usuario_id")
+	private Long usuarioId;
 }
