1ac64e1406a3619d7f9d6d718cd9b57ea9a3bc12
jhemesonmotta <jhemesonmotta@gmail.com>
2021-01-21 16:35:02 -0300

    service MedicaoPorEmpresaService

--- src/main/java/com/jhemeson/msprodutividade/service/MedicaoPorEmpresaService.java
+++ src/main/java/com/jhemeson/msprodutividade/service/MedicaoPorEmpresaService.java
@@ -1,7 +1,50 @@
 package com.jhemeson.msprodutividade.service;
 
+import com.jhemeson.msprodutividade.dto.General.MessageResponseDTO;
+import com.jhemeson.msprodutividade.dto.MedicaoPorEmpresa.MedicaoPorEmpresaDTO;
+import com.jhemeson.msprodutividade.entity.MedicaoPorEmpresa;
+import com.jhemeson.msprodutividade.mapper.MedicaoPorEmpresaMapper;
+import com.jhemeson.msprodutividade.repository.MedicaoPorEmpresaRepository;
+import javassist.NotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MedicaoPorEmpresaService {
+	private MedicaoPorEmpresaRepository medicaoPorEmpresaRepository;
+	private final MedicaoPorEmpresaMapper medicaoPorEmpresaMapper = MedicaoPorEmpresaMapper.INSTANCE;
+
+	@Autowired
+	public MedicaoPorEmpresaService(MedicaoPorEmpresaRepository medicaoPorEmpresaRepository){
+		this.medicaoPorEmpresaRepository = medicaoPorEmpresaRepository;
+	}
+
+	public MessageResponseDTO create(MedicaoPorEmpresaDTO medicaoPorEmpresaDTO) {
+		MedicaoPorEmpresa medicaoPorEmpresa = medicaoPorEmpresaMapper.toModel(medicaoPorEmpresaDTO);
+		medicaoPorEmpresaRepository.save(medicaoPorEmpresa);
+
+		return MessageResponseDTO.builder().
+				message("Medição criada com sucesso.")
+				.build();
+	}
+
+	public MedicaoPorEmpresaDTO findById(Long id) throws NotFoundException {
+		MedicaoPorEmpresa medicaoPorEmpresa = medicaoPorEmpresaRepository.findById(id).orElseThrow(() -> new NotFoundException("Medição não encontrada."));
+		return medicaoPorEmpresaMapper.toDTO(medicaoPorEmpresa);
+	}
+
+	public MessageResponseDTO update(MedicaoPorEmpresaDTO medicaoPorEmpresaDTO) {
+		MedicaoPorEmpresa medicaoPorEmpresa = medicaoPorEmpresaMapper.toModel(medicaoPorEmpresaDTO);
+		medicaoPorEmpresaRepository.save(medicaoPorEmpresa);
+
+		return MessageResponseDTO.builder().
+				message("Medição atualizada com sucesso.")
+				.build();
+	}
+
+	public List<MedicaoPorEmpresa> findAll() {
+		return medicaoPorEmpresaRepository.findAll();
+	}
 }
