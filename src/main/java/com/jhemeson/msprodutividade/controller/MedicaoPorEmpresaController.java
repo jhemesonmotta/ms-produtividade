5cf2bf8048929243247cfa98f036b482dfa32ebd
jhemesonmotta <jhemesonmotta@gmail.com>
2021-01-22 08:02:54 -0300

    add base dos rest controllers

---
+++ src/main/java/com/jhemeson/msprodutividade/controller/MedicaoPorEmpresaController.java
@@ -0,0 +1,9 @@
+package com.jhemeson.msprodutividade.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/medicao/empresa")
+public class MedicaoPorEmpresaController {
+}
